abe230403d044350ebeb95d059b153d67b557c61
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-01-19 11:46:40 +0100

    construction function

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 'use strict';
+
+// Constructor Function
+
+const Person = function (firstName, birthYear) {
+  (this.firstName = firstName), (this.birthYear = birthYear);
+};
+
+const jonas = new Person('Jonas', 1991);
+console.log(jonas);
+
+// Calling the function with new follows 4 steps thus
+// 1. new {}(empty object) is created
+// 2. function is called. this ={} (this keyword is assigned to the empty object)
+// 3. {} linked to the prototype
+// 4. function automatically returns the {}
