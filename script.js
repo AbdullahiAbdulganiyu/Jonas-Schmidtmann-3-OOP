8c19763a84487ec61159d1bc02183a0c9a49dc23
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-01-20 09:34:51 +0100

    Practice Object.create

--- script.js
+++ script.js
@@ -200,3 +200,27 @@ console.log(account.latest);
 console.log((account.latest = 500));
 
 console.log(account.movement);
+
+// Object.create
+
+const PersonProto = {
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  },
+
+  init(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  },
+};
+
+const steven = Object.create(PersonProto);
+steven.name = 'Steven';
+steven.birthYear = 2002;
+
+steven.calcAge();
+
+const sarah = Object.create(PersonProto);
+
+sarah.init('Sarah', 2020);
+sarah.calcAge();
